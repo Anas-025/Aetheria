f817e3e5634ed7369f95c27398780ca94acb4096
Anas-025 <200280107097@Ldceahm.gujgov.edu.in>
2023-07-07 00:58:48 +0530

    Trying to solve the serveless function timeout error | deleting the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,9 @@
-import Main from "components/home/Main/Main";
-import Footer from "../../components/home/Footer/Footer";
-import Hero from "../../components/home/Hero/Hero";
-import Services from "../../components/home/Services/Services";
-import SignIn from "../../components/signin/SignIn/SignInPage";
 
 export default function Home() {
   
   return (
     <>
-      <Hero />
-      <Main />
-      <Services />
-      <div style={{ marginTop: "8rem" }}>
-        <SignIn />
-      </div>
-      <Footer />
+      Home Page
     </>
   );
 }
